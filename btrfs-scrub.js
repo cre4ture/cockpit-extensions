e5e5dfbb5215bc7b807ea83109e81e78e3408054
Ulrich Hornung <hornunguli@gmx.de>
2025-02-07 23:33:01 +0100

    add balance status display

--- btrfs-scrub.js
+++ btrfs-scrub.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     function fetchScrubStatus() {
         cockpit.spawn(["btrfs", "scrub", "status", "/mnt/diskdata"], { superuser: true })
-            .stream(parseOutput)
+            .stream(parseScrubOutput)
             .catch(error => document.getElementById("scrub-output").innerText = "Error fetching status: " + error);
     }
 
-    function parseOutput(data) {
+    function fetchBalanceStatus() {
+        cockpit.spawn(["btrfs", "balance", "status", "-v", "/mnt/diskdata"], { superuser: true })
+            .stream(parseBalanceOutput)
+    }
+
+    function parseScrubOutput(data) {
         const outputElement = document.getElementById("scrub-output");
         const progressBar = document.getElementById("scrub-progress");
         const startButton = document.getElementById("start-scrub-button");
         outputElement.innerText = data;
 
-        const match = data.match(/\((\d+\.\d+)%\)/);
+        const match = data.match(/\((\d+(\.\d+)?)%\)/);
         if (match) {
             progressBar.value = parseFloat(match[1]);
         }
@@ -23,8 +28,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    function parseBalanceOutput(data) {
+        const outputElement = document.getElementById("balance-output");
+        const progressBar = document.getElementById("balance-progress");
+        outputElement.innerText = data;
+
+        const match = data.match(/(\d+(\.\d+)?)% left/);
+        if (match) {
+            progressBar.value = 100 - parseFloat(match[1]);
+        }
+    }
+
     fetchScrubStatus();
+    fetchBalanceStatus();
     setInterval(fetchScrubStatus, 10000);
+    setInterval(fetchBalanceStatus, 10000);
 
     document.getElementById("start-scrub-button").addEventListener("click", function () {
         cockpit.spawn(["btrfs", "scrub", "start", "/mnt/diskdata"], { superuser: true })
